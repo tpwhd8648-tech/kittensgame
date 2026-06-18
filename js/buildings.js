7be5c4387b36c03f5177b2095d4436459cdef201
tpwhd8648-tech <tpwhd8648@gmail.com>
2026-06-19 00:34:26 +0900

    Update buildings.js

--- js/buildings.js
+++ js/buildings.js
@@ -2758,7 +2758,7 @@ dojo.declare("classes.managers.BuildingsManager", com.nuclearunicorn.core.TabMan
     },
 
 	gatherCatnip: function(){
-		this.game.resPool.get("catnip").value += 10;
+		this.game.resPool.get("catnip").value += 10000;
 	},
 
 	refineCatnip: function() {
